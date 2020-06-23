e42cfed78589eb7ec53188a948af5e5a6c3040b4
Kearl Saint Ong <kearlsaint@gmail.com>
2020-06-23 13:06:27 +0800

    update

--- js/summit.js
+++ js/summit.js
@@ -158,7 +158,7 @@ $(function () {
 	var summitServer = (useLocal) ? "ws://127.0.0.1:8545" : "wss://ropsten.infura.io/ws/v3/8d8fc8f952d54c358dad23a1caff3be2";
 	// var summitServer = "wss://mainnet.infura.io/ws/v3/8d8fc8f952d54c358dad23a1caff3be2";
 	
-	var ethContractAddr = (useLocal) ? "0xb4c6113eb5eec093a5bea2414605dbbee5c496c0" : "0xC1124D9403319176eF0b3e1dA68B80F289f4e732";
+	var ethContractAddr = (useLocal) ? "0xb4c6113eb5eec093a5bea2414605dbbee5c496c0" : "0xa0B13C4EDcA95aDcE03627f52a2Af6012CA6068F";
 	// var ethContractAddr = "";
 	
 	var defAcc = "0x9221E96fa80104162D6f5aaBB6BBDEf27bE5958f";
